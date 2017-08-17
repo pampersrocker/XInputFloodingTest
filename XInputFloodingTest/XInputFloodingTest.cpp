d77c48b960d9499fb04b39d7916caf5de5617d26
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2017-08-17 05:01:50 +0200

    Added mutex to eliminate Race Conditions upon accessing Intensity.

--- XInputFloodingTest/XInputFloodingTest.cpp
+++ XInputFloodingTest/XInputFloodingTest.cpp
@@ -8,14 +8,17 @@
 volatile bool RequestedExit = false;
 volatile WORD Intensity = 0;
 volatile DWORD InputUpdateRate = 33; // Default to 33ms
+HANDLE IntensityMutex;
 
 DWORD WINAPI InputLoop(void* data)
 {
 	while (!RequestedExit)
 	{
 		XINPUT_VIBRATION Vibration;
+		WaitForSingleObject(IntensityMutex, INFINITE);
 		Vibration.wLeftMotorSpeed = Intensity;
 		Vibration.wRightMotorSpeed = Intensity;
+		ReleaseMutex(IntensityMutex);
 		for (DWORD ControllerId = 0; ControllerId < XUSER_MAX_COUNT; ++ControllerId)
 		{
 			DWORD Result = XInputSetState(ControllerId, &Vibration);
@@ -29,7 +32,9 @@ DWORD WINAPI UpdateLoop(void* data)
 {
 	while (!RequestedExit)
 	{
+		WaitForSingleObject(IntensityMutex, INFINITE);
 		Intensity = Intensity == 0 ? 32767 : 0;
+		ReleaseMutex(IntensityMutex);
 		// Sleep for 0.5s
 		Sleep(500);
 	}
@@ -41,9 +46,10 @@ DWORD WINAPI UpdateLoop(void* data)
 int main()
 {
 	HANDLE ThreadHandles[2];
+	IntensityMutex = CreateMutex(nullptr, false, nullptr);
 	ThreadHandles[0] = CreateThread(nullptr, 0, UpdateLoop, nullptr, 0, nullptr);
 	ThreadHandles[1] = CreateThread(nullptr, 0, InputLoop, nullptr, 0, nullptr);
-	if (ThreadHandles[0] && ThreadHandles[1])
+	if (ThreadHandles[0] && ThreadHandles[1] && IntensityMutex)
 	{
 		printf("Update rate set to 33ms\n");
 
@@ -56,6 +62,9 @@ int main()
 			{
 				RequestedExit = true;
 				WaitForMultipleObjects(2, ThreadHandles, true, INFINITE);
+				CloseHandle(ThreadHandles[0]);
+				CloseHandle(ThreadHandles[1]);
+				CloseHandle(IntensityMutex);
 			}
 			else
 			{
